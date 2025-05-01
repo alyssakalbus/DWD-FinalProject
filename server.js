8b121d16ff5e48ca6a3512addb0ac991aeae0aca
alyssakalbus <alyssakalbus@gmail.com>
2025-05-01 12:13:18 -0400

    added api endpoints and adjusted scaffolding

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const express = require('express');
 const app = express();
-const port = 3000;
+const config = require('./config.js');
 
-// Add middleware to parse JSON and form data
+const port = config.PORT || 3000;
+
+// Handle data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+const publicURL = path.resolve(`${__dirname}/public`);
 
+// server
+app.use(express.static(publicURL));
+
+// serve static files from the 'public' directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
@@ -16,8 +22,8 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/about.html'));
 });
 
-app.get('/careers', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/careers.html'));
+app.get('/services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/services.html'));
 });
 
 app.get('/lab', (req, res) => {
@@ -48,6 +54,52 @@ app.post('/login', (req, res) => {
     res.json({ message: 'Login functionality coming soon' });
 });
 
+// API Endpoints w/ CRUD operations
+
+app.get("/api/v1/finalproject", async (req, res) => {
+    try {
+        const data = await finalproject.find();
+        res.json({})
+    } catch (error) {
+        console.error(error);
+        res.json(error);
+    }
+});
+
+app.post("/api/v1/finalproject", async (req, res) => {
+    try {
+        const newData = {
+            // EDIT THIS
+            todo: req.body.todo,
+            status: req.body.status
+        }
+        const data = await todos.create(newData);
+        res.json({})
+    } catch (error) {
+        console.error(error);
+        res.json(error);
+    }
+});
+
+app.put("/api/v1/finalproject", async (req, res) => {
+    try {
+        res.json({})
+    } catch (error) {
+        console.error(error);
+        res.json(error);
+    }
+});
+
+app.delete("/api/v1/finalproject", async (req, res) => {
+    try {
+        res.json({})
+    } catch (error) {
+        console.error(error);
+        res.json(error);
+    }
+});
+
+// listen on port
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
